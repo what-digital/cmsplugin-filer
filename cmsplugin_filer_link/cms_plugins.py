3384769179020648a1da8b008c1ae197c8052032
dvanser <ivans.dorohovs@gmail.com>
2025-07-24 22:34:55 +0300

    replace ugettext with gettext

--- cmsplugin_filer_link/cms_plugins.py
+++ cmsplugin_filer_link/cms_plugins.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.templatetags.static import static
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
